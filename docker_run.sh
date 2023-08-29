64cabc2c4f4d76b74fd073c5cfa2042f67c8814a
Likalto04 <ricardo.montoya.da@gmail.com>
2023-08-29 20:23:01 +0200

    running 7GB docker image

--- docker_run.sh
+++ docker_run.sh
@@ -7,4 +7,4 @@ docker run \
     -v /home/ricardo/ABUS2023_documents/tdsc_abus23/input:/opt/usuari/input/ \
     -v /home/ricardo/ABUS2023_documents/tdsc_abus23/exit:/opt/usuari/predict/ \
     --shm-size 8g \
-    segmenter:proba
+    segmenter-min:proba
